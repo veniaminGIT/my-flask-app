20dd6c6a41fc2143e6d4a06fc42e769d66982c5d
veniaminGIT <mariusp86@yahoo.com>
2026-01-11 13:14:19 +0200

    versiune 1.0.3

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     return jsonify({
         "message": "Bine ai venit la aplicația mea Docker!",
         "timestamp": datetime.datetime.now().isoformat(),
-        "version": "1.0.2"
+        "version": "1.0.3"
     })
 
 @app.route('/health')
